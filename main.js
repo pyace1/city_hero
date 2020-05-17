47ab5357c90df20c3f412a2604df9ce3e1571b2f
Your Name <you@example.com>
2020-05-17 15:48:22 +0200

    nista novo

---
+++ main.js
@@ -0,0 +1,9 @@
+var heroji = [];
+function unos(){
+	var heroj = {
+		ime = document.getElementById("name").value,
+		grad = document.getElementById("city").value,
+		delo = document.getElementById("delo").value,
+	}
+	heroji.push(heroj);
+}

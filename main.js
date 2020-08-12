9b3508d878339c48959b12fb7f41f9f25a100263
Your Name <you@example.com>
2020-08-12 22:53:29 +0200

    novi css

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-var dt = new Date();
-document.getElementById("datetime").innerHTML = dt.toLocaleString();
+var myVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+  var d = new Date();
+  var t = d.toLocaleTimeString();
+  var date = d.getDate();
+  var month = d.getMonth();
+  var year = d.getFullYear();
+  document.getElementById("datetime").innerHTML = date + "/" + month + "/" + year + "<hr>" + t;
+ 
+}
+// --- forma unos ---
 var heroji = [];
 function unos(){
 	var heroj = {
-		ime = document.getElementById("name").value,
-		grad = document.getElementById("city").value,
-		delo = document.getElementById("delo").value,
+		ime : document.getElementById("name").value,
+		grad : document.getElementById("city").value,
+		delo : document.getElementById("delo").value,
 	}
 	heroji.push(heroj);
+	upis(heroji);
+}
+function upis(){
+	document.getElementById("upis").innerHTML = "<div>" + heroji[i].ime + " " + 
+	heroji[i].grad + "<br>" + heroji[i].delo + "</div><br><hr>";
+	
 }
